7b3e8de59f9e2681626c03f6d69c8147fdc3c563
Yuri <yuricaetano100@gmail.com>
2025-01-29 09:12:41 -0300

    configurando netlifly

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Ativa o modo estrito do React (opcional)
+    reactStrictMode: true,
+    output: 'standalone',
   };
   
   export default nextConfig;
